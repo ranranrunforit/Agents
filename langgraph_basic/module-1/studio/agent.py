d9ff92c0c7db01ae75a3e4a85a8fa2a6a8a5c043
ranranrunforit <ranranrunforit@users.noreply.github.com>
2025-08-27 14:52:39 +0800

    Add files via upload

--- langgraph_basic/module-1/studio/agent.py
+++ langgraph_basic/module-1/studio/agent.py
@@ -1,5 +1,6 @@
 from langchain_core.messages import SystemMessage
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 from langgraph.graph import START, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition, ToolNode
@@ -34,7 +35,11 @@ def divide(a: int, b: int) -> float:
 tools = [add, multiply, divide]
 
 # Define LLM with bound tools
-llm = ChatOpenAI(model="gpt-4o")
+#llm = ChatOpenAI(model="gpt-4o")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
 llm_with_tools = llm.bind_tools(tools)
 
 # System message
